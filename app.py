4e077ce33d95c6491d9cc6a3a0c0cb79e2fda80e
Max Salazar <maxsalazar@Creatives-iMac.local>
2022-06-16 21:42:46 -0700

    added input question

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-age = 26;
-first_name = "John";
-last_name = "Doe";
+age = input("How old are you? ");
+first_name = input("What is your first name? ");
+last_name = input("What is your last name? ");
 
-hello = "Hello " + first_name;
+hello = "Hello " + first_name + " " + last_name + " you are " + age + " years old.";
 
 print(hello + "!");
