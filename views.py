0a00fb79cf19b3a3814dd192cc74ab3e0fc1e9cf
Max Salazar <maxsalazar@Creatives-iMac.local>
2022-06-18 00:37:21 -0700

    added a little bit of css

--- views.py
+++ views.py
@@ -5,7 +5,7 @@ views = Blueprint(__name__, 'views')
 
 @views.route('/')
 def index():
-    return render_template('index.html', title='Home Page')
+    return render_template('index.html', title='Welcome to the Tax Calculator')
 
 @views.route('/calculator')
 def calculator():
